925a822ab493f44d4d64bd2aae7f572f2d2614b1
FukeKazki <kazkichi0906@gmail.com>
2024-07-21 15:32:04 +0900

    :tada: キャッシュをする

--- app/action.ts
+++ app/action.ts
@@ -5,29 +5,51 @@ import { createDoc, getDocsWithQuery } from "@/firebase-client/utils";
 import { OmitBase } from "@/schema/base";
 import { Prompt } from "@/schema/prompt";
 import { orderBy, query, where } from "firebase/firestore";
+import { revalidateTag, unstable_cache } from "next/cache";
 
 export async function getPromptsAction() {
-  return refs.pipe(
-    query(refs.promptsRef, orderBy("createdAt", "desc")),
-    getDocsWithQuery,
-  );
+  return unstable_cache(
+    () =>
+      refs.pipe(
+        query(refs.promptsRef, orderBy("createdAt", "desc")),
+        getDocsWithQuery,
+      ),
+    ["get-prompts"],
+    {
+      tags: ["get-prompts"],
+    },
+  )();
 }
 
 export async function getPromptsByTagAction(tagTitle: string) {
-  return refs.pipe(
-    query(
-      refs.promptsRef,
-      where("tags", "array-contains", tagTitle),
-      orderBy("createdAt", "desc"),
-    ),
-    getDocsWithQuery,
-  );
+  return unstable_cache(
+    () =>
+      refs.pipe(
+        query(
+          refs.promptsRef,
+          where("tags", "array-contains", tagTitle),
+          orderBy("createdAt", "desc"),
+        ),
+        getDocsWithQuery,
+      ),
+    ["get-prompts", tagTitle],
+    {
+      tags: ["get-prompts", tagTitle],
+    },
+  )();
 }
 
 export async function getTagsAction() {
-  return refs.pipe(refs.tagsRef, getDocsWithQuery);
+  return unstable_cache(
+    () => refs.pipe(refs.tagsRef, getDocsWithQuery),
+    ["get-tags"],
+    {
+      tags: ["get-tags"],
+    },
+  )();
 }
 
 export async function createPromptAction(data: OmitBase<Prompt>) {
-  return refs.pipe(refs.promptsRef, (x) => createDoc(x, data));
+  await refs.pipe(refs.promptsRef, (x) => createDoc(x, data));
+  revalidateTag("get-prompts");
 }
